2abf61f31f51fc8d5779979c419c4b1c77ff0004
Hayyan Al-Sayyed <hayyansayyed2@gmail.com>
2026-02-06 02:25:51 +0300

    modification

--- src/components/Home components/Our Product/data/data.js
+++ src/components/Home components/Our Product/data/data.js
@@ -1,7 +1,10 @@
-/**
- * 1. Products Section Header Data
- * Contains the main heading, description, and toggle button labels.
- */
+
+import ourproduct1st from "/Images/HomePage/Checking Account.png";
+import ourproduct2nd from "/Images/HomePage/Saving Account.png";
+import ourproduct3rd from "/Images/HomePage/Loans and Mortgages.png";
+import ourproduct4th from "/Images/HomePage/check.png";
+import ourproduct5th from "/Images/HomePage/save.png";
+import ourproduct6th from "/Images/HomePage/loans.png";
 
 export const OurProductsHeaderData =
 {
@@ -13,50 +16,47 @@ export const OurProductsHeaderData =
 }
 
 
-/**
- * 2. Products List Data
- * Array of banking products categorized by 'individual' and 'businesses'.
- */
+
 export const OurProductsData = [
   {
     id: 1,
     category: "individual",
-    icon: "/Images/HomePage/Checking Account.png",
+    icon: { ourproduct1st },
     title: "Checking Accounts",
     description: "Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access."
   },
   {
     id: 2,
     category: "individual",
-    icon: "/Images/HomePage/Saving Account.png",
+    icon: { ourproduct2nd },
     title: "Savings Accounts",
     description: "Build your savings with our competitive interest rates and flexible savings account options. Whether you're saving for a specific goal or want to grow your wealth over time, we have the right account for you."
   },
   {
     id: 3,
     category: "individual",
-    icon: "/Images/HomePage/Loans and Mortgages.png",
+    icon: { ourproduct3rd },
     title: "Loans and Mortgages",
     description: "Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need."
   },
   {
     id: 4,
     category: "businesses",
-    icon: "/Images/HomePage/check.png",
+    icon: { ourproduct4th },
     title: "Checking Accounts - Businesses",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex harum exercitationem sapiente suscipit ipsum repellat eligendi, accusamus vitae voluptatem! Praesentium illo autem dolor perferendis ab. Provident dolore blanditiis atque"
   },
   {
     id: 5,
     category: "businesses",
-    icon: "/Images/HomePage/save.png",
+    icon: { ourproduct5th },
     title: "Savings Accounts - Businesses",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex harum exercitationem sapiente suscipit ipsum repellat eligendi, accusamus vitae voluptatem! Praesentium illo autem dolor perferendis ab. Provident dolore blanditiis atque"
   },
   {
     id: 6,
     category: "businesses",
-    icon: "/Images/HomePage/loans.png",
+    icon: { ourproduct6th },
     title: "Loans and Mortgages - Businesses",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex harum exercitationem sapiente suscipit ipsum repellat eligendi, accusamus vitae voluptatem! Praesentium illo autem dolor perferendis ab. Provident dolore blanditiis atque"
   },
