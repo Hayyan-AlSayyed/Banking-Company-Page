19a7caf43a45106f633852c032cb2deda8c4d2ab
Hayyan Al-Sayyed <hayyansayyed2@gmail.com>
2026-02-06 04:31:12 +0300

    gg

--- src/components/Home components/Our Product/data/data.js
+++ src/components/Home components/Our Product/data/data.js
@@ -1,11 +1,4 @@
 
-import ourproduct1st from "./Images/HomePage/CheckingAccount.png";
-import ourproduct2nd from "./Images/HomePage/SavingAccount.png";
-import ourproduct3rd from "./Images/HomePage/LoansandMortgages.png";
-import ourproduct4th from "./Images/HomePage/check.png";
-import ourproduct5th from "./Images/HomePage/save.png";
-import ourproduct6th from "./Images/HomePage/loans.png";
-
 export const OurProductsHeaderData =
 {
   titleOne: "Our",
@@ -21,42 +14,42 @@ export const OurProductsData = [
   {
     id: 1,
     category: "individual",
-    icon: ourproduct1st,
+    icon: "./Images/HomePage/CheckingAccount.png",
     title: "Checking Accounts",
     description: "Enjoy easy and convenient access to your funds with our range of checking account options. Benefit from features such as online and mobile banking, debit cards, and free ATM access."
   },
   {
     id: 2,
     category: "individual",
-    icon: ourproduct2nd,
+    icon: "./Images/HomePage/SavingAccount.png",
     title: "Savings Accounts",
     description: "Build your savings with our competitive interest rates and flexible savings account options. Whether you're saving for a specific goal or want to grow your wealth over time, we have the right account for you."
   },
   {
     id: 3,
     category: "individual",
-    icon: ourproduct3rd,
+    icon: "./Images/HomePage/LoansandMortgages.png",
     title: "Loans and Mortgages",
     description: "Realize your dreams with our flexible loan and mortgage options. From personal loans to home mortgages, our experienced loan officers are here to guide you through the application process and help you secure the funds you need."
   },
   {
     id: 4,
     category: "businesses",
-    icon: ourproduct4th,
+    icon: "./Images/HomePage/check.png",
     title: "Checking Accounts - Businesses",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex harum exercitationem sapiente suscipit ipsum repellat eligendi, accusamus vitae voluptatem! Praesentium illo autem dolor perferendis ab. Provident dolore blanditiis atque"
   },
   {
     id: 5,
     category: "businesses",
-    icon: ourproduct5th,
+    icon: "./Images/HomePage/save.png",
     title: "Savings Accounts - Businesses",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex harum exercitationem sapiente suscipit ipsum repellat eligendi, accusamus vitae voluptatem! Praesentium illo autem dolor perferendis ab. Provident dolore blanditiis atque"
   },
   {
     id: 6,
     category: "businesses",
-    icon: ourproduct6th,
+    icon: "./Images/HomePage/loans.png",
     title: "Loans and Mortgages - Businesses",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex harum exercitationem sapiente suscipit ipsum repellat eligendi, accusamus vitae voluptatem! Praesentium illo autem dolor perferendis ab. Provident dolore blanditiis atque"
   },
