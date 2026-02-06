c0e01ec2f253acc2229a979e7682d8d2fbfcf285
Hayyan Al-Sayyed <hayyansayyed2@gmail.com>
2026-02-06 05:22:16 +0300

    dfsdfdgssd

--- src/components/Security components/HowProtectYouData.js
+++ src/components/Security components/HowProtectYouData.js
@@ -1,24 +1,21 @@
-
-
 export const CardData = [{
     id: 1,
     imag: "/Banking-Company-Page/Images/SecurityPage/SecureOnlineBankingPlatform.png",
-
     title: "Secure Online Banking Platform",
     paragraph: "Our online banking platform is built with multiple layers of security to safeguard your information. We utilize industry-standard encryption protocols to ensure that your data remains confidential and protected during transmission."
 }, {
     id: 2,
-    imag: "/Banking-Company-Page//Images/SecurityPage/Multi-FactorAuthentication.png",
+    imag: "/Banking-Company-Page/Images/SecurityPage/Multi-FactorAuthentication.png",
     title: "Multi-Factor Authentication",
     paragraph: "To enhance the security of your online banking experience, we employ multi-factor authentication. This additional layer of security requires you to provide multiple pieces of identification, such as a password and a one-time verification code, to access your account."
 }, {
     id: 3,
-    imag: "/Banking-Company-Page//Images/SecurityPage/FraudMonitoring.png",
+    imag: "/Banking-Company-Page/Images/SecurityPage/FraudMonitoring.png",
     title: "Fraud Monitoring",
     paragraph: "We have sophisticated fraud detection systems in place to monitor your accounts for any suspicious activities. Our dedicated team works around the clock to detect and prevent unauthorized transactions, providing you with peace of mind."
 }, {
     id: 4,
-    imag: "/Banking-Company-Page//Images/SecurityPage/SecureMobileBanking.png",
+    imag: "/Banking-Company-Page/Images/SecurityPage/SecureMobileBanking.png",
     title: "Secure Mobile Banking",
     paragraph: "Our mobile banking app is designed with the same level of security as our online banking platform. You can confidently access your accounts, make transactions, and manage your finances on the go, knowing that your information is protected."
 }]
