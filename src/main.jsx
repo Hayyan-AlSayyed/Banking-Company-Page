be25dc67c46aa273581c01c61eb5e402d379fe4c
Hayyan Al-Sayyed <hayyansayyed2@gmail.com>
2026-02-06 03:12:10 +0300

    C

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/Banking-Company-Page/'>
+    <BrowserRouter basename='/Banking-Company-Page'>
       <App />
     </BrowserRouter>
   </StrictMode>,
